de8b83f4024316f6bacd55b50444af93f5af4ad6
Fillbary <p.sobako@yandex.ru>
2018-07-12 21:31:10 +0300

    Удалил лишнее

--- Homework#4_easy2.py
+++
@@ -1,8 +0,0 @@
-# Задание-2:
-# Даны два списка фруктов.
-# Получить список фруктов, присутствующих в обоих исходных списках.
-
-list_l = ['banana', 'orange', 'apple', 'melon']
-list_m = ['qiwi', 'banana', 'cucumber', 'pineapple', 'apple']
-list_t = list (set (list_l) & set (list_m))
-print(list_t)
